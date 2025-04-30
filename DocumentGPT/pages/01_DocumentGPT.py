314555ed89875383008e10ad9e353f3a13b3ba91
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2025-04-30 15:09:03 +0900

    Add files via upload

--- DocumentGPT/pages/01_DocumentGPT.py
+++ DocumentGPT/pages/01_DocumentGPT.py
@@ -1,28 +1,107 @@
 import streamlit as st
-import time
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma, FAISS
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+
+chat = ChatOpenAI(
+    #                 model="gpt-4.1-nano",
+    temperature=0.1,
+    tiktoken_model_name="gpt-3.5-turbo",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()])
+
+@st.cache_data(show_spinner= "Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./cache/embeddings/{file.name}")
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+def send_message(message, role,save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message":message, "role":role})
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"],message["role"],save=False)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
+
+st.markdown("""
+Welcome!
+
+Use this chatbot to ask question to an AI about your files!
+
+Upload your file on the sidebar.
+""")
+
+with st.sidebar:
+    file = st.file_uploader("Upload a .txt or .docx file",type = ["txt","docx"])
+
+if file:
+    retriever = embed_file(file)
+    send_message("I'm ready! Ask away", "ai",save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+
+    if message:
+        send_message(message, "human")
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question" : RunnablePassthrough(),
+        } | prompt | chat
+
+        # same as chain
+        # docs = retriever.invoke(message)
+        # # docs = "\n\n".join(document.page_content for document in docs)
+        # prompt = prompt.format_messages(context=docs, question = message)
+        # chat.predict_messages(prompt)
+
+        response = chain.invoke(message)
+        send_message(response.content,"ai")
+
+else:
     st.session_state["messages"] = []
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message":message,"role":role})
-
-
-for message in st.session_state["messages"]:
-    send_message(message["message"],message["role"],False)
-
-
-message = st.chat_input("Send a message to the ai")
-
-if message:
-    send_message(message,"human")
-    time.sleep(2)
-    send_message(f"You said: {message}","ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
